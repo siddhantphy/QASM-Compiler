fbc80dec94866a7e78344d36651b53ec3c1fdeab
Siddhant Singh <ssingh8@tudelft.nl>
2022-01-31 16:12:57 +0100

    Siddhant

--- QASM_oop.py
+++ QASM_oop.py
@@ -1,13 +1,28 @@
 import numpy as np
+# from typing import list
 
 class QASM_compiler():
-    
+
+    self.nq: int
+    self.operations: dict
+
     def __init__(self, file_p: str):
         self.QASM_read(filepath=file_p)
 
+    @property
+    def read_circuit(self):
+        return self.read_circuit
+
+    @read_circuit.setter
     def QASM_read(self, filepath: str):
         self.read_circuit = open(filepath, 'r')
         self.read_circuit.read()
+
+    def initialize_register(self):
+        pass
+
+    def gates(self):
+        pass
 
     def simulate_circuit(self):
         pass
